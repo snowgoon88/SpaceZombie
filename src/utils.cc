b1d4470aa83d75ede697511ec6eba2a934b2bb06
Alain <snowgoon88@gmail.com>
2014-01-14 20:21:13 +0100

    DOC : added src/utils.h/cc

--- src/utils.cc
+++ src/utils.cc
@@ -1,1 +1,87 @@
-../../Inertia/src/utils.cc
+// -*- coding: utf-8 -*-
+
+#include "utils.h"
+
+/****************************************************************** LINE_REPR */
+std::string line_repr( Eigen::VectorXf vector, int precision)
+{
+  std::stringstream ss;
+
+  for( int i=0; i < vector.size(); i++) {
+    ss << SETPREC(precision) << (vector)(i) << " ";
+  }
+  
+  return ss.str();
+}
+/*******************************************************************CONVERSION*/
+float to_deg( float rad )
+{
+  float deg = fmod( (rad * 180.0 / M_PI), 360.0);
+  if( deg > 180.0) deg = deg - 360.0;
+  if( deg < -180.0) deg = deg + 360.0;
+  return deg;
+}
+float to_rad( float deg )
+{
+  float rad = fmod( (deg * M_PI / 180.0), 2*M_PI);
+  if( rad > M_PI) rad = rad - (2*M_PI);
+  if( rad < -M_PI) rad = rad + (2*M_PI);
+  return rad;
+}
+float clip_rad_pi( float rad )
+{
+  float res = fmod( rad, 2*M_PI);
+  if( res > M_PI) res = res - (2*M_PI);
+  if( res < -M_PI) res = res + (2*M_PI);
+  return res;
+}
+float clip_rad_2pi( float rad )
+{
+  float res = fmod( rad, 2*M_PI);
+  if( res > 2 * M_PI) res = res - (2*M_PI);
+  if( res < 0) res = res + (2*M_PI);
+  return res;
+}
+// ****************************************************************** VEC TO ANG
+void ang_from_vec( TVec3 v, float& ang_z1, float& ang_y2 )
+{
+  // Projection on Oxy
+  TVec3 vxy = v;
+  vxy(2) = 0;
+  if( vxy.norm() > 0.0001 ) {
+    // Angle Ox,vxy
+    TVec3 ux( 1, 0, 0);
+    ang_z1 = acosf( ux.dot( vxy ) / vxy.norm() / ux.norm());
+    // Check for the right sign
+    if( vxy(1) < 0 ) ang_z1 = -ang_z1;
+    
+    // Angle vxy v
+    float co = vxy.dot( v ) / v.norm() / vxy.norm();
+    if( co > 1 ) co = 1.0f;
+    if( co < -1 ) co = -1.0f;
+    ang_y2 = - acosf( co );
+    // if( isnan(ang_y2) ) {
+    //   std::cout << "@@@@@@@@ ang_from_vec() ang_y2 is nan!" << "\n";
+    //   std::cout << "v=" << line_repr(v) << " vxy=" << line_repr(vxy) << "\n";
+    //   std::cout << "ang_z1=" << ang_z1 << " acos_of=" << vxy.dot( v ) / v.norm() / vxy.norm() << "\n";
+    // }
+    // Check for the right sign
+    if( v(2) < 0 ) ang_y2 = -ang_y2;
+  }
+  else {
+    ang_z1 = 0;
+    if( v(2) > 0 ) {
+      ang_y2 = - M_PI / 2.0;
+    }
+    else {
+      ang_y2 = M_PI / 2.0;
+    }
+  }
+}
+/******************************************************************************/
+TwStructMember SVec3Members[] = {
+    { "x", TW_TYPE_FLOAT, offsetof(SVec3, x), "Step=0.1" },
+    { "y", TW_TYPE_FLOAT, offsetof(SVec3, y), "Step=0.1" },
+    { "z", TW_TYPE_FLOAT, offsetof(SVec3, z), "Step=0.1" }
+};
+// *****************************************************************************
--- src/utils.h
+++ src/utils.h
@@ -1,1 +1,84 @@
-../../Inertia/src/utils.h
+// -*- coding: utf-8 -*-
+/** @file utils.h
+ * Various utilities.
+ *
+ * + MACRO for formating float with precision
+ * + Angle conversion and clipping.
+ * + Euler angles from a TVec3 vector
+ * + Structure for using a (x,y,z) vector in AntTweakBar
+ *
+ * @author Alain
+ * @date 01/2014
+ */
+
+#ifndef __UTILS_H_
+#define __UTILS_H_
+
+#include "math.h"
+#include <Eigen/Dense>
+// AntTweakBar
+#include <AntTweakBar.h>
+
+// Eigen library for "dense" matrices and vectors
+#include <Eigen/Dense>
+#include <vector>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+// Précision dans les formats
+#include <iomanip>
+/**
+ * MACRO precision of 'p' for float.
+ */
+#define SETPREC(P) std::setiosflags(std::ios::fixed) << std::setprecision(P)
+/**
+ * Vector types:
+ * + TVec2 and TVec2Ptr : vector with 2 dimensions.
+ * + TVec3 and TVec3Ptr : vector with 2 dimensions.
+ */
+typedef Eigen::Vector2f TVec2;
+typedef Eigen::Vector3f TVec3;
+#ifdef HAVE_BOOST
+#include <boost/shared_ptr.hpp>
+typedef boost::shared_ptr<TVec3> TVec3Ptr;
+typedef boost::shared_ptr<TVec2> TVec2Ptr;
+#else
+typedef TVec3 * TVec3Ptr;
+typedef TVec2 * TVec2Ptr;
+#endif
+
+/**
+ * Return a str line representation of a vector.
+ */
+std::string line_repr( Eigen::VectorXf vector, int precision=2);
+
+/** Angle conversion in [-180, 180] */
+float to_deg( float rad );
+/** Angle conversion in [-M_PI, M_PI] */
+float to_rad( float deg );
+/** Angle Clip to [-M_PI,M_PI] */
+float clip_rad_pi( float rad);
+/** Angle Clip to [0,2*M_PI] */
+float clip_rad_2pi( float rad);
+
+/** From a TVec3, get 2 first euler angles : 0x->vxy and vxy->v */
+void ang_from_vec( TVec3 v, float& ang_z1, float& ang_y2 );
+
+/** AntTweakBar structure for TVec3 */
+// A user defined structure
+typedef struct {
+  float x; float y; float z;
+} SVec3;
+
+/**
+ * Struc to be used by AntTweakBar.
+ *
+ * + x : float, step by 0.1
+ * + y : float, step by 0.1
+ * + z : float, step by 0.1
+ */
+extern TwStructMember SVec3Members[];
+
+#endif // __UTILS_H_
